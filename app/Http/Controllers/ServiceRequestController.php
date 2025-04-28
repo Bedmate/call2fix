33bfdaac171edabb479920f23b3a0d5639cc13a2
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-28 23:22:03 +0100

    added _account_type filter on retrieving service requests

--- app/Http/Controllers/ServiceRequestController.php
+++ app/Http/Controllers/ServiceRequestController.php
@@ -47,7 +47,7 @@ class ServiceRequestController extends Controller
     public function index()
     {
         try {
-            $serviceRequests = ServiceRequestModel::with('reworkMessages', 'service_provider', 'invited_artisan')->whereUserId(auth()->id())->orderBy('updated_at', 'desc')->get();
+            $serviceRequests = ServiceRequestModel::with('reworkMessages', 'service_provider', 'invited_artisan')->where('_account_type', active_role())->whereUserId(auth()->id())->orderBy('updated_at', 'desc')->get();
             return get_success_response($serviceRequests);
         } catch (\Throwable $th) {
             return get_error_response($th->getMessage());
