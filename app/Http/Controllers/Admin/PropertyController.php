dcb7f158ff99c6b55f5b871c958ff53547794021
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-28 19:54:19 +0100

    debuggin view property

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -76,6 +76,7 @@ class PropertyController extends Controller
 
     public function show(Property $property)
     {
+        return response()->json($property);
         return view('admin.properties.show', compact('property'));
     }
 
