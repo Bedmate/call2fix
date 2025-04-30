00e8b0eaa279f76e64f2ef1f27ab0ece5605bb2e
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-04-30 10:51:01 +0000

    added service ratings to user model - debugging

--- app/Models/User.php
+++ app/Models/User.php
@@ -336,8 +336,8 @@ class User extends Authenticatable
 
     public function getRatingsAttribute()
     {
-        $pluckIds = ServiceRequestModel::where('approved_providers_id', $this->id)->pluck('id');
-        return $getRatings = ServiceRequestRatings::whereIn('service_request_id', $pluckIds)->get();
+        return $pluckIds = ServiceRequestModel::where('approved_providers_id', $this->id)->pluck('id');
+        $getRatings = ServiceRequestRatings::whereIn('service_request_id', $pluckIds)->get();
 
         $total = 0;
         $count = 0;
