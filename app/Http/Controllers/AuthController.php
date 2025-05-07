1d2810cb23c5f0231f96831c45a30ecc40db03c5
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-05-07 20:07:20 +0100

    debugging ref code validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -667,7 +667,7 @@ class AuthController extends Controller
         try {
             $ref_code = $request->referred_by;
             $referrer = Referral::userByReferralCode($ref_code);
-            return response()->json(['response' => Referral::all()]);
+            // return response()->json(['response' => Referral::all()]);
             if (!$referrer) {
                 return get_error_response('Referrer not found', ['is_valid_referrer' => false]);
             }
