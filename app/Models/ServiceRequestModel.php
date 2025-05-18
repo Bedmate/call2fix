8ff51298300a21fa1fdab4b7e529b983d4b7a571
EMMANUEL TOWOJU <40261626+towoju5@users.noreply.github.com>
2025-05-18 16:13:24 +0000

    carbon imported into the service request model

--- app/Models/ServiceRequestModel.php
+++ app/Models/ServiceRequestModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Services\GeoLocationService;
+use Carbon;
 
 class ServiceRequestModel extends Model
 {
